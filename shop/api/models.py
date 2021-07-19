3ea01dc982bf963b2b186770ab614c756e22cba8
MOM — Maks <mom-maks@MacBook-Air-MOM-2.local>
2021-07-19 17:37:15 +0500

    [Update] Updated models)

--- shop/api/models.py
+++ shop/api/models.py
@@ -8,13 +8,13 @@ class Product(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
     type = models.ManyToManyField('ProductType', related_name='product')
     cost = models.DecimalField(max_digits=10, blank=True, null=True, decimal_places=2, default=0.00)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class ProductType(models.Model):
     type_uid = models.IntegerField(blank=False, null=False, unique=True)
     type_name = models.CharField(max_length=50, blank=False, null=False, unique=True)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
